5e48957820e87cbe8c669e917a44e3b939c0530d
gauravpatil-06 <gp949958@gmail.com>
2026-08-31 21:30:00 +0530

    student dashboard added

---
+++ src/main/java/com/student/servlet/student/StudentServlet.java
@@ -0,0 +1,20 @@
+package com.student.servlet.student;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/student")
+public class StudentServlet extends HttpServlet
+{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        response.sendRedirect(request.getContextPath() + "/student/dashboard.jsp");
+    }
+}
